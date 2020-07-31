12713bf7dccc434f0e46d1bf2bd63e7a60b68592
Abdul Aliyev <aliyev_abdul@bah.com>
2020-07-31 15:50:46 -0400

    Add error handling pages

--- forum_blog/__init__.py
+++ forum_blog/__init__.py
@@ -1,6 +1,8 @@
 from forum_blog.core.views import core
+from forum_blog.error_pages.handlers import error_pages
 from flask import Flask
 
 app = Flask(__name__)
 
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
