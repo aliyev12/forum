bd9accf1358e93b214bc3bfcc7a69bfa9626b5cc
Abdul Aliyev <aliyev_abdul@bah.com>
2020-07-31 16:22:41 -0400

    Add BlogPost model

--- forum_blog/models.py
+++ forum_blog/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from forum_blog import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -34,4 +35,20 @@ class User(db.Model, UserMixin):
 
 
 class BlogPost(db.Model):
-    pass
+
+    users = db.relationship(User)
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Post ID: {self.id} -- Date: {self.date} -- {self.title}"
