db5b55e012dce551f1403f406072323ce9bbe69a
Abdul Aliyev <aliyev_abdul@bah.com>
2020-07-31 15:59:24 -0400

    Set up local db

--- forum_blog/__init__.py
+++ forum_blog/__init__.py
@@ -1,8 +1,26 @@
-from forum_blog.core.views import core
 from forum_blog.error_pages.handlers import error_pages
+from forum_blog.core.views import core
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+
+################################
+######## Database Setup ########
+################################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+################################
+
 
 app.register_blueprint(core)
 app.register_blueprint(error_pages)
---
+++ forum_blog/models.py
@@ -0,0 +1,9 @@
+from forum_blog import db
+
+
+class User(db.Model):
+    pass
+
+
+class BlogPost(db.Model):
+    pass
